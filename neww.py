f4add1805638bdd4909c26b2900104ed22cacec8
agastyaan <agastyaantechnology@gmail.com>
2025-09-04 11:19:45 +0530

    wel.py

--- neww.py
+++ neww.py
@@ -1,12 +1,6 @@
 import streamlit as s
-import pandas as pd
-import matplotlib.pyplot as plt
+
 s.title("Stock Marketing App")
 s.write("Fetch and visualize stock price data")
-s.image("https://images.pexels.com/photos/31009096/pexels-photo-31009096.jpeg")
 
-data = pd.DataFrame({'symbol':['AAPL','OANDA','AMZN','TSLA','FXCM','SPY','BINANCE','ARKK'],'PRICE':[163.33,1.13419,2898.89,950.545,1.88693,440.96,38069.18,72.25]})
-
-plt.plot(data["symbol"],data["PRICE"])
-s.pyplot(plt)
 
